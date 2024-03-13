ef866600339494d59288dd82efa15961318d287f
MagicTINTIN <valentinservieres@gmail.com>
2024-03-13 17:23:18 +0100

    fix, it works with base map

--- infinitecrafter.js
+++ infinitecrafter.js
@@ -1,35 +1,62 @@
 BASE_RECIPE_BOOK = {
-    water:[],
-    fire:[],
-    wind:[],
-    earth:[]
+    Water: [],
+    Fire: [],
+    Wind: [],
+    Earth: []
 };
 
 recipeBook = {
-    water:[],
-    fire:[],
-    wind:[],
-    earth:[]
+    Water: [],
+    Fire: [],
+    Wind: [],
+    Earth: []
 };
 
 tested = [];
 
 newWords = [];
 
+function isArrayInArray(array, item1, item2) {
+    for (const craft of array) {
+        if ((craft[0] == item1 && craft[1] == item2) || (craft[1] == item1 && craft[0] == item2))
+            return true;
+    }
+    return false;
+}
+
 function isTested(item1, item2) {
-    return (tested.includes({item1, item2}) || tested.includes({item2, item1}))
+    return isArrayInArray(tested, item1, item2);
 }
 
 function addTested(item1, item2) {
-    tested.push({item1, item2});
+    if (!isTested(item1, item2))
+        tested.push([ item1, item2 ]);
+}
+
+function isTestedAndAddIt(item1, item2) {
+    let isTestedRes = isArrayInArray(tested, item1, item2);
+    if (!isTestedRes)
+        tested.push([ item1, item2 ]);
+    return isTestedRes;
 }
 
 function addReceipe(res, item1, item2) {
     if (!recipeBook[res])
-        recipeBook[res] = [{item1, item2}];
+        recipeBook[res] = [[ item1, item2 ]];
     else {
-        if (!recipeBook[res].includes({item1, item2}) && !recipeBook[res].includes({item2, item1}))
-            recipeBook[res].push({item1, item2});
+        if (!isArrayInArray(recipeBook[res], item1, item2));
+            recipeBook[res].push([ item1, item2 ]);
+    }
+}
+
+async function fetchPageAndReturnJSON(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching the page:', error);
+        return null;
     }
 }
 
@@ -38,7 +65,19 @@ function searchWords(numberOfAllCombinaisonsPerWord) {
     for (const key in recipeBook) {
         wordsToTest.push(key);
     }
-    for (const iterator of object) {
-        
+    for (const item1 of wordsToTest) {
+        for (const item2 of wordsToTest) {
+            if (!isTestedAndAddIt(item1, item2)) {
+                fetchPageAndReturnJSON(`https://neal.fun/api/infinite-craft/pair?first=${item1}&second=${item2}`)
+                    .then(jsonData => {
+                        //console.log('JSON data:', jsonData);
+                        if (jsonData.result != "Nothing" && jsonData.emoji != "")
+                            addReceipe(jsonData.result, item1, item2);
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                    });
+            }
+        }
     }
 }
